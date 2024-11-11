7788f1f2976f4102eac38f7df61858495d227c82
maxence <maxence.largeot@epitech.eu>
2024-11-11 17:06:52 +0100

    feat(images/ | Dockerfile | docker-compose): root docker file & compose located previously in jenkins folder + c base & standalone Dockerfile

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -4,3 +4,13 @@ folder('/Whanos base images') {
 folder('/Projects') {
     displayName('Projects')
 }
+
+languages = ["c"]
+
+languages.each { language ->
+    freeStyleJob("/Whanos base images/whanos-$language") {
+        steps {
+            shell("docker build -t whanos-$language -f /images/$language/Dockerfile.base /images/$language")
+        }
+    }
+}
