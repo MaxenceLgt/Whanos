b8cba2db068cb542193be5ea0e02aa860e9a8866
maxence <maxence.largeot@epitech.eu>
2024-11-18 11:24:05 +0100

    refactor(job_dsl.groovy): Fix whanos build all base images

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -5,12 +5,24 @@ folder('/Projects') {
     displayName('Projects')
 }
 
-languages = ["c", "java", "javascript"]
+languages = ["c", "java", "javascript", "python", "befunge"]
 
 languages.each { language ->
     freeStyleJob("/Whanos base images/whanos-$language") {
         steps {
             shell("docker build -t whanos-$language -f /images/$language/Dockerfile.base /images/$language")
+        }
+    }
+}
+
+freeStyleJob("Whanos base images/Build all base images") {
+    steps {
+        publishers {
+            downstream (
+                languages.collect { language ->
+                    "/Whanos base images/whanos-$language"
+               }
+            )
         }
     }
 }
